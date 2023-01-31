34b0c7a1a28d639633dbf30497daf68db7d21362
KevinRoost <kroost92@gmail.com>
2023-01-31 18:49:24 -0500

    create About component and NavBar, stub up all pages

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,19 @@
+import './About.css'
+
+function About () {
+  return(
+    <>
+      <h1>About</h1>
+      <img src="images/headshot.jpg" alt="headshot" id="headshot"/>
+      <p>
+        My journey so far has been a wild ride. I’ve been a theater maker for most of my life. (Still am!) I started out as an actor, then eventually a teacher/choreographer, and recently a director/producer. This unique industry demands teamwork, cultivates camaraderie, and tests problem-solving on the fly. Theater has always been and always will be my biggest passion. 
+
+        Simultaneously, I used to serve and bartend. While I was always ready to leave the service industry, I do appreciate that it shares much of the list of necessary skills with theater; namely dealing with people and quick problem solving.  
+
+        Come 2020, I had an excuse to leave the service industry and find a different way to channel those skills. It took some time, but I finally discovered a way to do this AND lean into my strength in math and logic- coding! I took the dive and graduated from the immersive bootcamp with General Assembly, during which I got hands on experience with React, Express, JS, HTML, CSS, and more. My ongoing goal is to apply the seemingly unrelated strengths and skills I have cultivated throughout my life to my daily coding endeavors. I can finally say I’m thoroughly enjoying what I do everyday, whether it is theater or tech!
+      </p>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home () {
+  return(
+    <>
+      <h1>Kevin Roost</h1>
+      <h3>Back-End Developer</h3>
+      <p>img</p>
+      
+    </>
+  )
+}
+
+export default Home
