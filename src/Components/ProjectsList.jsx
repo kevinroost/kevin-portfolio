e2f45320a3e145489818790a828f3ed6fa778ecd
KevinRoost <kroost92@gmail.com>
2023-02-01 18:29:31 -0500

    fix silly typo hehe

--- src/Components/ProjectsList.jsx
+++ src/Components/ProjectsList.jsx
@@ -3,7 +3,9 @@ const ProjectsList = ({project}) => {
     <>
       <li>
         <h3>{project.title}</h3>
-        <img src={project.image} alt={`Screenshot of ${project.title}`} />
+        <p>
+          {project.description}
+        </p>
       </li>
     </>
   )
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,21 @@
 const projects = [
   {
     title: "Tic-Tac-Toe",
-    discription: "My first project as a developer, this is a simple recreation of the classic game Tic-Tac-Toe, using JS with a dash of CSS and HTML. The program takes in each player's name and addresses them throughout, all the way up to announcing the winner.",
+    description: "My first project as a developer, this is a simple recreation of the classic game Tic-Tac-Toe, using JS with a dash of CSS and HTML. The program takes in each player's name and addresses them throughout, all the way up to announcing the winner.",
     image: "/images/TicTacToe.png",
     repositoryLink: "https://github.com/kevinroost/ttt-weekend.git",
     deploymentLink: "This project isn't deployed yet!"
   },
   {
     title: "Boss Battles",
-    discription: "**STILL IN DEVELOPMENT** Boss Battles is a text based battle simulator that pits players against 5 different enemies, gathering loot along the way. After the player chooses one of three classes, each with unique starting stats, they come face-to-face with their first opponent. Once defeated, the boss drops 3 pieces of equipment that the player will choose from. Each piece of equipment will boost certain stats upon pick-up, setting up the player for the next battle. Victory comes when all 5 bosses are defeated!",
+    description: "**STILL IN DEVELOPMENT** Boss Battles is a text based battle simulator that pits players against 5 different enemies, gathering loot along the way. After the player chooses one of three classes, each with unique starting stats, they come face-to-face with their first opponent. Once defeated, the boss drops 3 pieces of equipment that the player will choose from. Each piece of equipment will boost certain stats upon pick-up, setting up the player for the next battle. Victory comes when all 5 bosses are defeated!",
     image: "/images/BossBattles.png",
     repositoryLink: "https://github.com/kevinroost/BossBattles.git",
     deploymentLink: "https://kevinroost-bossbattles.netlify.app/"
   },
   {
     title: "Fantasy Drag Race",
-    discription: "**STILL IN DEVELOPMENT** Finally! An app that lets you and your friends get together and create a fantasy league... for Ru Paul's Drag Race! After all the queens have been introduced, in the case of this season, after episode 2, any number of people can join and draft a team of 5 queens. As the episodes air, each queen's total points will go up (or down!) depending on her performance in the episode. Users can even suggest additional ways to earn points, but only before the teams are finalized.",
+    description: "**STILL IN DEVELOPMENT** Finally! An app that lets you and your friends get together and create a fantasy league... for Ru Paul's Drag Race! After all the queens have been introduced, in the case of this season, after episode 2, any number of people can join and draft a team of 5 queens. As the episodes air, each queen's total points will go up (or down!) depending on her performance in the episode. Users can even suggest additional ways to earn points, but only before the teams are finalized.",
     image: "/images/FantasyDragRace.png",
     repositoryLink: "https://github.com/kevinroost/Fantasy-Drag-Race.git",
     deploymentLink: "https://fantasy-drag-race.fly.dev/"
