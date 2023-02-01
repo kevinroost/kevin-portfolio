e54e81f8a30e3613efc624c62c86e735d6db7b13
KevinRoost <kroost92@gmail.com>
2023-01-31 19:24:26 -0500

    build static pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,20 @@ function Contact () {
   return(
     <>
       <h1>Contact</h1>
+      <h3>Say "Hey!"</h3>
+      <p>
+        You can email me <a href="mailto:kroost92@gmail.com">here</a>,
+      </p>
+      <p>
+        connect with me on <a href="https://www.linkedin.com/in/kevinroost/">LinkedIn</a>,
+      </p>
+      <p>
+        or even give me a call at 561-843-0291
+      </p>
+      <h3>Check out my work!</h3>
+      <a href="https://github.com/kevinroost">GitHub</a>
+        <br></br>
+      <a href="./projects">Projects</a>
     </>
   )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,12 @@
+import { Document, Page } from "react-pdf"
+
 function Resume () {
   return(
     <>
       <h1>Resume</h1>
-      
+      <p>
+        Grab my <a href="images/KevinRoostResumePDF.pdf">resume</a>
+      </p>
     </>
   )
 }
